78056af84d5a1e97f295a1ed2563f8bb32585a2a
Nicolas Denier <nicodenier@laposte.net>
2023-11-17 12:54:03 +0100

    Loading functions cached with mongodb

--- mongo/mongo.py
+++ mongo/mongo.py
@@ -39,3 +39,54 @@ def df_to_mongo(db, collection, df):
     d = df_to_dict(df)
     push_to_collection(db, collection, d)
 
+def transform_tiles_from_mongo(df):
+    '''
+    Transform the tiles zoning data from mongodb to convert it back to a dataframe
+    Args:
+        df (pd.DataFrame): the df coming directly from mongo, that needs to be transformed
+    '''
+    prefix = 'work'
+    # extract work sub dict to df
+    df = pd.concat([df, pd.DataFrame.from_records(df[prefix].to_list()).add_prefix(prefix+'_')], axis=1)
+    df = df.drop(columns=prefix)
+    df = df.rename(columns={'_id': 'h3', 'nuts-3': 'nuts3', 'shape': 'geometry', 'work_total': 'work'})
+    return df
+
+def transform_regions_from_mongo(df):
+    '''
+    Transform the demographic data from mongodb to convert it back to a dataframe
+    '''
+    prefixes = [c for c in df.columns if c in ['age', 'vehicle', 'employment_rate', 'employment_type']]
+    # extract the sub dicts
+    df = pd.concat([df]+[pd.DataFrame.from_records(df[p].to_list()).add_prefix(p+'_') for p in prefixes], axis=1)
+    df = df.drop(columns=prefixes)
+    df = df.rename(columns={'_id': 'nuts3'})
+    return df
+
+def search(db, collection, match_field ,match_ids, fields):
+    '''
+    Search if the region_ids are in the db and returns the values for given fields
+    Args:
+        collection (str): name of the mongo collection
+        match_field (str): the mongo field name to match from (ex: 'nuts-3')
+        match_ids (list of str): list of ids to match (ex: region ids)
+        fields (list of str): list of the names of the collection field to retrieve
+    Returns:
+        pd.DataFrame
+    '''
+    if collection=='tiles':
+        match_dict = {
+            'nuts-3': 'nuts3',
+            '_id': 'h3',
+        }
+    elif collection=='regions':
+        match_dict = {
+            '_id': 'nuts3',
+        }
+
+    fields_query = {f:1 for f in fields}
+    result = db[collection].find( { match_field: { '$in': match_ids } }, fields_query )
+    df = pd.DataFrame.from_records(result)
+    df = df.rename(columns=match_dict)
+    return df
+

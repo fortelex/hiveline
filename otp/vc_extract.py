ec96e63560c73e4c4d7da9b0139892029fc38f87
Vector-Hector <erik.doering@t-online.de>
2023-11-19 19:00:27 +0100

    fixed vc_extract date format

--- otp/vc_extract.py
+++ otp/vc_extract.py
@@ -102,7 +102,7 @@ def extract_traveller(vc):
     # check if created is a string
     if type(created) == str:
         # convert to datetime
-        created = datetime.strptime(created, "%Y-%m-%d %H:%M:%S.%f")
+        created = datetime.strptime(created, "%Y-%m-%d %H:%M:%S")
 
     return {
         "employed": vc["employed"],
